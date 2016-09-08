fc46db37e36b685b77e9df5cea35d9984fad47fb
cassieyrh <cassieyrh@users.noreply.github.com>
2016-09-08 19:42:06 -0400

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -7,11 +7,7 @@ public class Solution {
         if(p.length()==0) return s.length()==0;
         else{
             if(p.length() ==1 || p.charAt(1) != '*'){
-                if (s.isEmpty() || (p.charAt(0) != '.' && p.charAt(0) != s.charAt(0))) {
-                    return false;
-                } else {
-                    return isMatch(s.substring(1), p.substring(1));
-             }
+                return s.length() != 0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0)) && isMatch(s.substring(1), p.substring(1));
             }else{
                 return isMatch(s, p.substring(2)) || s.length()!=0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') && isMatch(s.substring(1), p);
             }

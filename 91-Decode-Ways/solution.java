8876313717efee98ff58bcb4eeecf193bfa1e4da
cassieyrh <cassieyrh@users.noreply.github.com>
2016-10-06 10:40:19 -0400

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,17 +1,20 @@
 public class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length()==0 || s.charAt(0)=='0') return 0;
-        
-        int dp1 = 1, dp2 = 1; // dp1: decode ways of s.substring(0,i), dp2: decode ways of s.substring(0, i-1)
-        
-        for (int i = 1; i < s.length(); i++){
-            if (s.charAt(i)=='0') dp1 = 0;
-            
-            if (s.charAt(i-1) == '1' || s.charAt(i-1) == '2' && s.charAt(i) <= '6'){
-                dp1 = dp1 + dp2;
-                dp2 = dp1-dp2;
-            }else dp2 = dp1;
+        int len = s.length();
+        if(len == 0 || s.charAt(0) == '0') return 0;
+        int[] dp = new int[len];
+        dp[0] = 1;
+        for(int i = 1; i < len; i++){
+            if(s.charAt(i) > '9' || s.charAt(i) < '0') return 0;
+            if(s.charAt(i) == '0'){
+                if(s.charAt(i-1) != '1' && s.charAt(i-1) != '2') return 0;
+                dp[i] = i > 1 ?dp[i-2] : 1;
+                if(i > 1) dp[i-1] = dp[i-2];
+            } 
+            else if (s.charAt(i) <= '6' && s.charAt(i) > '0' && s.charAt(i-1) == '2' || s.charAt(i-1) == '1'){
+                dp[i] = dp[i-1] + (i > 1? dp[i-2] : 1);
+            }else dp[i] = dp[i-1];
         }
-        return dp1;
+        return dp[len-1];
     }
 }
